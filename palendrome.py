d5620f1c170f89e204c677004b8c49030644effe
katelucas <lucas.katie@gmail.com>
2019-06-05 09:36:51 -0400

    what i got

---
+++ palendrome.py
@@ -0,0 +1,12 @@
+sample1 = input("Enter your text to determine if it is a palendrome: ")
+sample1.split (" ")
+# To be done:
+# 1. convert sample by implimenting .lower, remove punctuation, convert to list by .split by " "
+# 2. duplicate sample list, invert sample list
+# 3. if sample list == sample list2 - palendrome
+# 4. if sample list =/= sample list2 - not a palendrome
+
+
+print("This is not a palendrome")
+
+
